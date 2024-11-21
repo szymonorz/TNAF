266b582420106ece6d1b35f735c5aa8edf616fa2
szymonorz <szyorz@proton.me>
2024-11-21 08:35:33 +0100

    Zad1: functions.js

---
+++ zad1/functions.js
@@ -0,0 +1,13 @@
+function isOdd(x) {
+    return x % 2 == 1;
+}
+
+const isEven = (x) => {
+    return x % 2 == 0;
+}
+
+
+console.log(isOdd(1))
+console.log(isEven(1))
+console.log(isOdd(2))
+console.log(isEven(2))
